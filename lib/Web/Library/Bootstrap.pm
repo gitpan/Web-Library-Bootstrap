663b66c75d4da9568dbcb67be3de61a5be2fba6d
Marcel Grünauer <marcel@cpan.org>
2013-08-11 19:15:44 +0000

    Import of MARCEL/Web-Library-Bootstrap-0.04 from CPAN.
    gitpan-cpan-distribution: Web-Library-Bootstrap
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MARCEL/Web-Library-Bootstrap-0.04.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Web/Library/Bootstrap.pm
+++ lib/Web/Library/Bootstrap.pm
@@ -1,7 +1,7 @@
 package Web::Library::Bootstrap;
 use Moose;
 with qw(Web::Library::Provider Web::Library::SimpleAssets);
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 sub latest_version { '2.3.2' }
 
 sub version_map {
